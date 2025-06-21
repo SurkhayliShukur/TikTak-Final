1662f32a8aa6db22fd045da8c15d0c57ba7688f9
SurkhayliShukur <shukursurkhayli000@gmail.com>
2025-06-21 15:35:00 +0400

    feat: remove unused App.css

--- tiktak-admin/src/App.tsx
+++ tiktak-admin/src/App.tsx
@@ -1,11 +1,13 @@
 
+import React from 'react';
+import Navbar from './common/components/Navbar';
 
 function App() {
  
-
   return (
     <>
-    <div>Hello World</div>
+    <Navbar/>
+    <div className="underline">Hello World</div>
     </>
   )
 }
--- tiktak-admin/src/common/components/Navbar/index.tsx
+++ tiktak-admin/src/common/components/Navbar/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
    <>
-   
+   <div className='w-full h-[6rem] bg-white flex justify-around items-center' >
+     
+   </div>
    </>
   )
 }
