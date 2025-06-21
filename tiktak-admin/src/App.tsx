2c48833af6bee89987b263a8485aa8fd2a565a69
SurkhayliShukur <shukursurkhayli000@gmail.com>
2025-06-21 16:28:07 +0400

    feat: update Navbar component layout

--- tiktak-admin/src/App.tsx
+++ tiktak-admin/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
     <Navbar/>
-    <div className="underline">Hello World</div>
     </>
   )
 }
--- tiktak-admin/src/common/components/Navbar/index.tsx
+++ tiktak-admin/src/common/components/Navbar/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-   <>
-   <div className='w-full h-[6rem] bg-white flex justify-around items-center' >
-     
-   </div>
-   </>
-  )
-}
+    <>
+      <div className="w-full h-[6rem] bg-white flex justify-evenly items-center">
+        <h1 className="uppercase text-800 text-[40px]">Tik Tak Admin</h1>
+        <input
+          type="text"
+          placeholder="Axtarış"
+          className="w-[588px] h-[45px] px-[25px] py-[13px] rounded-xl outline-none bg-[var(--main-color)] "
+        />
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
